33f1c2f46fc6f3c9bc2f9a8ba79f8cc85f2a3277
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-01-11 23:37:06 +0100

    Preprocessing handler mappings to fit better into WADL

--- src/main/scala/com/blogspot/nurkiewicz/web/WadlController.scala
+++ src/main/scala/com/blogspot/nurkiewicz/web/WadlController.scala
@@ -7,22 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 import scala.collection.JavaConversions._
 import net.java.dev.wadl._2009._02._
+import javax.servlet.http.HttpServletRequest
 
 /**
  * @author Tomasz Nurkiewicz
  * @since 09.01.12, 22:39
  */
 @Controller
-class WadlController @Autowired() (mapping: RequestMappingHandlerMapping) {
+class WadlController @Autowired()(mapping: RequestMappingHandlerMapping) {
 
-	@RequestMapping(value= Array("/"), method = Array(GET))
-	@ResponseBody def generate() = {
-		mapping.getHandlerMethods.keys.foreach{m =>
-			println(m.getMethodsCondition + " => " + m.getPatternsCondition)
-		}
+	@RequestMapping(value = Array("/"), method = Array(GET))
+	@ResponseBody def generate(request: HttpServletRequest) = {
+
+		val methods = for (mappingInfo <- mapping.getHandlerMethods.keys;
+		                   method <- mappingInfo.getMethodsCondition.getMethods;
+		                   pattern <- mappingInfo.getPatternsCondition.getPatterns)
+			yield (pattern, method)
+		methods.groupBy(_._1).collect({case (k, v) => (k, v.map(_._2))}).toSeq.sortBy(_._1.size) foreach println
+
 		val application = new WadlApplication
 		val wadlResources = new WadlResources
-		wadlResources.setBase("http://...")
+		wadlResources.setBase(request.getRequestURL.toString)
 		val wadlResource = new WadlResource
 		val wadlMethod = new WadlMethod
 		wadlMethod.setName("GET")
